a40555e0eabaecb442b521fb8c0adba949b98cc1
Guido P <gperdomo@intellisys.com.do>
2018-06-14 16:44:03 -0400

    Initialize

---
+++ src/components/Global/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <p>&#169; Copyright de la maldita página</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
